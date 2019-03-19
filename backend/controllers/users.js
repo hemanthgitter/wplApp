a89c5c355a2040af101d6bb2b2e3cb4b8521ac51
hemanthgitter <hemanth.bharan92@gmail.com>
2019-03-19 01:15:29 -0500

    Added role based and token authentication

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/users');
 
 module.exports = {
@@ -26,11 +27,23 @@ module.exports = {
         let result = {};
         let status = 200;
         const {email, password} = req.body;
-        
+        console.log("Email: ", email);
+        console.log("Password: ", password);
         User.findOne({email}, (err, user) => {
             if(!err && user){
                 bcrypt.compare(password, user.password).then(match => {
                     if(match){
+                        let payload;
+                        if(user.email === 'admin@gmail.com'){
+                            payload = { "user": user.email, "roles": ["admin"]};
+                        }else{
+                            payload = { "user": user.email, "roles": ["user"]};
+                        }
+                        const options = { "expiresIn": "2d"};
+                        const secret = process.env.JWT_SECRET;
+                        const token = jwt.sign(payload, secret, options);
+
+                        result.token = token;
                         result.status = status;
                         result.result = user;
                     }else{
@@ -51,5 +64,29 @@ module.exports = {
                 result.error = err;
             }
         });
+    },
+
+    getAll: (req, res) => {
+        let result = {};
+        let status = 200;
+        const payload = req.decoded;
+        if(payload && payload.roles[0]==='admin'){
+            User.find({}, (err, users) => {
+                if(!err){
+                    result.status = status;
+                    result.result = users;
+                }else{
+                    status = 500;
+                    result.status = status;
+                    result.error = err;
+                }
+                res.status(status).send(result);
+            });
+        }else{
+            status = 401;
+            result.status = status;
+            result.error = `Authentication error`;
+            res.status(status).send(result);
+        }
     }
 }
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,13 @@
 const controller = require('../controllers/users');
+const validateToken = require('../utils').validateToken;
 
 module.exports = (router) => {
-    router.route('/users')
+    router.route('/register')
         .post(controller.add);
 
     router.route('/login')
         .post(controller.login);
+
+    router.route('/users')
+        .get(validateToken, controller.getAll);
 }
